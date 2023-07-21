f011b51a6a38300fcae703849c4adc102c03274d
Ian Flynn <ianflynnwork@gmail.com>
2023-07-21 15:43:57 -0400

    onclick open oauth window

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,10 +4,17 @@ import Login from './Login.jsx';
 
 const NavBar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
+  //create onclick function that opens google oauth modal
+  const googleLogin = () => {
+    console.log('google login');
+    window.open('http://localhost:3000/auth/google', '_self');
+  };
+
   return (
     <div id='navbar'>
       <img id='logo' src={logo} alt='Juno Surf' />
-      <button onClick={() => setMenuOpen(!menuOpen)}>Login</button>
+      {/* <button onClick={() => setMenuOpen(!menuOpen)}>Login</button> */}
+      <button onClick={googleLogin}>Login</button>
       <Login menuOpen={menuOpen} />
     </div>
   );
