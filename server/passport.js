b761076afed5d14326f3651861fb52e550a00601
Ian Flynn <ianflynnwork@gmail.com>
2023-07-21 15:42:10 -0400

    add de/serializeUser and move passport out of server file

---
+++ server/passport.js
@@ -0,0 +1,28 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+console.log('passport setting up');
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: process.env.GOOGLE_REDIRECT_URI,
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log('passport callback function fired');
+      // console.log(profile);
+      done(null, profile);
+    }
+  )
+);
+
+passport.serializeUser((user, done) => {
+  console.log('serializing user');
+  done(null, user);
+});
+passport.deserializeUser((user, done) => {
+  console.log('deserializing user');
+  done(null, user);
+});
+
+console.log('passport done setting up');
