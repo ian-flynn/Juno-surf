2a11caee0bbc62514ed4a5502ed4dfb99ea7736f
Ian Flynn <ianflynnwork@gmail.com>
2023-07-26 20:40:00 -0400

    added route to answer frontend with authenticated user info

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -6,7 +6,7 @@ const CLIENT_URL = 'http://localhost:8080/';
 router.get(
   '/google',
   passport.authenticate('google', {
-    scope: ['profile'],
+    scope: ['profile', 'email'],
   })
 );
 router.get(
@@ -19,5 +19,16 @@ router.get(
     res.redirect(CLIENT_URL);
   }
 );
+router.get('/user', (req, res) => {
+  console.log('authed?:  ', req.isAuthenticated());
+  console.log('fun def?:  ', `${req.isAuthenticated}`);
+  console.log('req.user: ', req.user);
+  res.json({ user: req.user });
+});
+
+router.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect(CLIENT_URL);
+});
 
 module.exports = router;
