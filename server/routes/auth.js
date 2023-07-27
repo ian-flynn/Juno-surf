131e03a84042f36fe2ff54bd50756d92ec6ab62a
Ian Flynn <ianflynnwork@gmail.com>
2023-07-27 17:37:50 -0400

    remove succuss redirect and logout work, session destroy and cookie removal WIP

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -4,31 +4,36 @@ const passport = require('passport');
 router.get(
   '/google',
   passport.authenticate('google', {
-    scope: ['profile', 'email'],
+    scope: ['profile'],
   })
 );
 router.get(
   '/google/callback',
   passport.authenticate('google', {
-    successRedirect: process.env.CLIENT_URL,
     failureRedirect: process.env.CLIENT_URL,
     session: true,
   }),
   (req, res) => {
-    res.redirect(CLIENT_URL);
+    return res.redirect(process.env.CLIENT_URL);
   }
 );
-// router.get('/user', (req, res) => {
-//   // console.log('req.session: ', req.session);
-//   // console.log('REQ: ', req);
-//   console.log('authed?:  ', req.isAuthenticated());
-//   console.log('req.user: ', req.user);
-//   res.send({ user: req.user });
-// });
 
-router.get('/logout', (req, res) => {
-  req.logout();
-  res.redirect(process.env.CLIENT_URL);
+router.post('/logout', (req, res, next) => {
+  console.log('logging out route hit');
+
+  req.logout((err) => {
+    if (err) return next(err);
+    req.session.destroy((err) => {
+      if (err) return next(err);
+      console.log('session destroyed');
+    });
+    return res
+      .status(200)
+      .clearCookie('connect.sid', {
+        path: '/',
+      })
+      .json('logged out');
+  });
 });
 
 module.exports = router;
