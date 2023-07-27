1f418aaf67962b6366725095af6a18d55035b1c8
Ian Flynn <ianflynnwork@gmail.com>
2023-07-27 16:13:54 -0400

    added credentials to fetch

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,25 +6,27 @@ const App = () => {
   const [user, setUser] = useState(null);
 
   //check if the user is logged in
-  useEffect(() => {
-    const fetchUser = async () => {
-      console.log('fetching user');
-      try {
-        const response = await fetch('http://localhost:3000/auth/user');
-        console.log('here');
-        // console.log(response);
-        const data = await response.json();
-        console.log('data', data);
-        if (data) {
-          console.log('user', `${data}`);
-          // setUser(data);
-        } else {
-          console.log('no user');
-        }
-      } catch (err) {
-        console.log(err);
+  const fetchUser = async () => {
+    try {
+      console.log(process.env.NODE_ENV);
+      const response = await fetch('http://localhost:3000/user', {
+        credentials: 'include',
+      });
+      // console.log('here');
+      // console.log(response);
+      const data = await response.json();
+      console.log('data', data);
+      if (data) {
+        // console.log('user', data);
+        // setUser(data);
+      } else {
+        console.log('no user');
       }
-    };
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  useEffect(() => {
     fetchUser();
   }, []);
 
@@ -33,6 +35,7 @@ const App = () => {
       <div id='app'>
         <NavBar user={user} />
         <SurfData />
+        <button onClick={fetchUser}>Fetch User</button>
       </div>
     </div>
   );
