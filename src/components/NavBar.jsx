c8f31d3a3e9ec8083bc2df9b76c9859b64b0f1e3
Ian Flynn <ianflynnwork@gmail.com>
2023-07-26 20:41:23 -0400

    changes to google login button to swap it back and forth with logout

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,20 +2,28 @@ import React, { useState } from 'react';
 import logo from '../images/JunoSurfLogo.png';
 import Login from './Login.jsx';
 
-const NavBar = () => {
-  const [menuOpen, setMenuOpen] = useState(false);
+const NavBar = ({ user }) => {
+  // const [menuOpen, setMenuOpen] = useState(false);
   //create onclick function that opens google oauth modal
   const googleLogin = () => {
-    console.log('google login');
     window.open('http://localhost:3000/auth/google', '_self');
   };
 
   return (
     <div id='navbar'>
       <img id='logo' src={logo} alt='Juno Surf' />
-      {/* <button onClick={() => setMenuOpen(!menuOpen)}>Login</button> */}
-      <button onClick={googleLogin}>Login</button>
-      <Login menuOpen={menuOpen} />
+      {user ? (
+        <>
+          {/* <p>{user}</p> */}
+          <button>Logout</button>
+        </>
+      ) : (
+        <button onClick={googleLogin}>
+          Google
+          <br />
+          Login
+        </button>
+      )}
     </div>
   );
 };
