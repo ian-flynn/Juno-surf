730e8f30ee8442d3be9b8f507ecb5c71a4fb2b96
Ian Flynn <ianflynnwork@gmail.com>
2023-07-28 13:25:33 -0400

    swap from withCredentials - true, to credentials include. Fixed cookie removal issue

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,24 +3,17 @@ import logo from '../images/JunoSurfLogo.png';
 import Login from './Login.jsx';
 
 const NavBar = ({ user, setUser }) => {
-  // const [menuOpen, setMenuOpen] = useState(false);
-  //create onclick function that opens google oauth modal
   const googleLogin = () => {
     window.open('http://localhost:3000/auth/google', '_self');
   };
   const googleLogout = async () => {
-    console.log('starting logout');
     const res = await fetch('http://localhost:3000/auth/logout', {
       method: 'POST',
-      withCredentials: true,
+      credentials: 'include',
     });
-    console.log('RESPONSE: ', res);
     const data = await res.json();
-    console.log('DATA: ', data);
-
     if (data === 'logged out') {
-      setUser(null);
-      // window.location.reload(true);
+      window.location.reload(true);
     }
   };
 
