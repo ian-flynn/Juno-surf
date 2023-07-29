0d569be6cbe11dffc540cc9202bbdfbd6e404228
Ian Flynn <ianflynnwork@gmail.com>
2023-07-29 11:58:02 -0400

    move components into own home route, style navbar that is seperated now

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import SurfData from '../components/SurfData.jsx';
+const Homepage = () => {
+  return (
+    <div className='pages homepage'>
+      <div id='app'>
+        <SurfData />
+      </div>
+    </div>
+  );
+};
+
+export default Homepage;
